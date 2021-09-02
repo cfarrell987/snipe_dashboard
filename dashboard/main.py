42ff2f9f1e33ffd84ad1130c2f4b73d2d41a4aeb
cfarrell987 <caleb.farrell987@gmail.com>
2021-09-01 21:04:16 -0300

    Changed get() to GET from SnipeIT dev demo

--- dashboard/main.py
+++ dashboard/main.py
@@ -4,9 +4,28 @@ import os
 
 def get(url):
     url = url
-    response = requests.get(url)
+
+    with open("api_key.txt", "r") as file:
+        api_key = file.read()
+
+    querystring = {
+        "limit": "2",
+        "offset": "0",
+        "sort": "model",
+        "order": "desc"
+    }
+    headers = {
+        "Accept": "application/json",
+        "Authorization": api_key.rstrip("'\n\"")
+    }
+
+    response = requests.request("GET",
+                                url,
+                                headers=headers,
+                                params=querystring)
+    print(headers)
     print(response.text)
 
 
 if __name__ == '__main__':
-    get("https://cdn.valhallahosting.ca/")
+    get("https://develop.snipeitapp.com/api/v1/hardware")
